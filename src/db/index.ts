535be94325dd7dc2a0ffecffa8eff63e26a50e42
Wahyu Akhmad Fadillah <Wahyufadil1140@gmail.com>
2026-02-04 22:05:01 +0700

    refactor: update database connection to use Neon serverless and adjust import paths for schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,20 @@
-import { config } from 'dotenv'
+// import { config } from 'dotenv'
 
 // import { drizzle } from 'drizzle-orm/node-postgres'
+// // import { drizzle } from 'drizzle-orm/neon-http'
+
+// import * as schema from './schema.ts'
+
+// config({ path: ['.env.local', '.env'] })
+
+// export const db = drizzle(process.env.DATABASE_URL!, { schema })
+import { config } from 'dotenv'
+import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 
-import * as schema from './schema.ts'
+import * as schema from './schema'
 
 config({ path: ['.env.local', '.env'] })
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema })
+const sql = neon(process.env.DATABASE_URL!)
+export const db = drizzle(sql, { schema })

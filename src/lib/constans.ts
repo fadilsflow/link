3532849d479cf341914f2ce5be75294d804cd649
Wahyu Akhmad Fadillah <Wahyufadil1140@gmail.com>
2026-02-09 09:08:03 +0700

    refactor(config): rename BASE_URL to VITE_BASE_URL for Vite compatibility

--- src/lib/constans.ts
+++ src/lib/constans.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.BASE_URL || 'http://localhost:3000'
+export const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3000'
 export const LINK_VERSION = '1.0.0'
 export const COMPANY_NAME = 'Karya.top'
 // export const COMPANY_NAME = 'Link.com, Inc.'

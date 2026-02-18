51190b25f21eb785dbfe56aa63f8334341eb4b4e
Wahyu Akhmad Fadillah <Wahyufadil1140@gmail.com>
2026-02-19 06:16:57 +0700

    style(header): update brand name and adjust layout spacing

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -28,13 +28,13 @@ export const Header = () => {
     <header className="sticky top-0 z-40 w-full bg-background  before:absolute before:inset-x-0 before:bottom-0 before:h-px before:bg-border/64">
       <nav
         // data-state={menuState && "active"}
-        className={'w-full transition-all duration-300'}
+        className={'container w-full transition-all duration-300'}
       >
         <div className="container py-4">
           <div className="flex flex-wrap items-center justify-between gap-6  lg:gap-0">
             <div className="flex items-center gap-6">
-              <Link className="mt-0.5 flex shrink-0 items-center gap-1.5 font-heading font-semibold   sm:text-2xl sm:text-[1.625em]" aria-label="Home" to="/">
-                Kreasi
+              <Link className="-mt-0.5 flex shrink-0 items-center gap-1.5 font-heading font-semibold   sm:text-2xl sm:text-[1.625em]" aria-label="Home" to="/">
+                Kreasi.top
               </Link>
               <Button variant="outline" render={<a href={REPO_URL} target='_blank' />} className="rounded-full flex justify-center "><GitHub className='invert' />5.3K <Star className='fill-current' /></Button>
             </div>

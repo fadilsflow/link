b62d593b06f6ac290aa6239219f318a14af20d59
Wahyu Akhmad Fadillah <Wahyufadil1140@gmail.com>
2026-02-21 00:51:10 +0700

    fix logo import

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -4,7 +4,7 @@ import { authClient } from "@/lib/auth-client";
 import { useState } from "react";
 import { Spinner } from '@/components/ui/spinner';
 import { useRouter } from '@tanstack/react-router';
-import { LogoMark } from '@/components/logo.tsx';
+import { LogoMark } from '@/components/logo';
 
 
 type AuthType = "login" | "register"

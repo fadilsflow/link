b5cace1555c19c9fcdd5a20df0ccbf7f298fa971
Wahyu Akhmad Fadillah <Wahyufadil1140@gmail.com>
2026-03-20 16:34:44 +0700

    refactor(ui): restructure footer to separate terms link from copyright

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -4,17 +4,16 @@ import { COMPANY_NAME } from '@/lib/constans'
 export function SiteFooter() {
   return (
     <footer className="relative py-6 text-muted-foreground ">
-      <div className="container mx-auto flex w-full items-center justify-center gap-2 px-4 sm:px-6">
+      <div className="container mx-auto flex w-full items-center justify-between gap-2 px-4 sm:px-6">
         <p>
           © {new Date().getFullYear()}{' '}
           <Link className="font-heading text-foreground text-lg" to="/">
             {COMPANY_NAME}
           </Link>{' '}
-          •{' '}
-          <Link className="underline" to="/tos">
-            Terms of Service
-          </Link>
         </p>
+        <Link className="underline" to="/tos">
+          Terms of Service
+        </Link>
       </div>
     </footer>
   )
